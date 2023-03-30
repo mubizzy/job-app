7ffff3f51ebf2e6116b630bc9497846c11c8d5fb
Mubarak <ajibolamubarak304@gmail.com>
2023-03-30 13:05:55 -0500

    search panel

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,29 +1,30 @@
-import React from 'react'
-import { View, Text,TextInput,TouchableOpacity,Image,FlatList } from 'react-native'
-import { useRouter } from 'expo-router'
-import styles from './welcome.style'
-import {icons,SIZES}from "../../../constants"
+import React from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+import styles from "./welcome.style";
+import { icons, SIZES } from "../../../constants";
 const Welcome = () => {
   return (
     <View>
-       <View style={styles.container}>
+      <View style={styles.container}>
         <Text style={styles.userName}>Hello Mubarak</Text>
         <Text style={styles.welcomeMessage}>Find your Dream job</Text>
-        </View>
+      </View>
 
-        <View style={styles.searchContainer} > 
-        <View style={styles.searchWrapper}>
-        <TextInput
-        style={styles.searchInput}>
-          </TextInput>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}> 
+          <TextInput style={styles.searchInput}></TextInput>
         </View>
-         <TextInput
-        style={styles.searchInput}>
-          </TextInput>
-          </View>
-     
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Welcome
+export default Welcome;
