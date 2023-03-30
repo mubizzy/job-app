0b0d468e7a1569b281ddf6bd76f5ba784d5cf533
Mubarak <ajibolamubarak304@gmail.com>
2023-03-30 04:31:40 -0500

    welcome page

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text,TextInput,TouchableOpacity,Image,FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
 import styles from './welcome.style'
-
+import {icons,SIZES}from "../../../constants"
 const Welcome = () => {
   return (
     <View>
-      <Text>Welcome</Text>
+       <View style={styles.container}>
+        <Text style={styles.userName}>Hello Adrian</Text>
+        <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+        </View>
+     
     </View>
   )
 }
