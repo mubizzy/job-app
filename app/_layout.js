df75a597f2a035335cd719720d5b0482e7f360c7
Mubarak <ajibolamubarak304@gmail.com>
2023-03-30 14:00:06 -0500

    layout setting 2

--- app/_layout.js
+++ app/_layout.js
@@ -1,5 +1,18 @@
 import { Stack } from "expo-router";
-
+import { useCallback } from "react";
+import { useFonts } from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
 export default function Layout() {
-  return <Stack />;
+  const [fontsLoaded] = useFonts({
+    DMBold: require("../assets/fonts/DMSans-Bold.tff"),
+    DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  return <Stack onlayout={onLayoutRootView} />;
 }
