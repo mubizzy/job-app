8dbce80dde9a5fa266059b8948124bb28b8fecaa
Mubarak <ajibolamubarak304@gmail.com>
2023-03-31 18:30:35 -0500

    popular job3

--- app/_layout.js
+++ app/_layout.js
@@ -3,15 +3,5 @@ import { useCallback } from "react";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 export default function Layout() {
-  const [fontsLoaded] = useFonts({
-    // DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
-    // DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
-  });
-
-  // const onLayoutRootView = useCallback(async () => {
-  // if (fontsLoaded) {
-  // await SplashScreen.hideAsync();
-  // }
-  // }, [fontsLoaded]);
   return <Stack />;
 }
