3220a4e399d918ea95fc55e981ea0d043b87c343
Mubarak <ajibolamubarak304@gmail.com>
2023-03-31 18:28:56 -0500

    popular job2

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 
-import styles from './popularjobs.style'
+import { useRouter } from "expo-router";
+import { icons, SIZES } from "../../../constants";
+import styles from "./popularjobs.style";
 
 const Popularjobs = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>Popularjobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popularjobs</Text>
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Popularjobs
+export default Popularjobs;
