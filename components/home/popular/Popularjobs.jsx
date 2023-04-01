da59e327195f62e6077144736caea8d09f25b4a6
Mubarak <ajibolamubarak304@gmail.com>
2023-03-31 19:01:27 -0500

    popular job4

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -9,15 +9,24 @@ import {
 } from "react-native";
 
 import { useRouter } from "expo-router";
-import { icons, SIZES } from "../../../constants";
+import { COLORS, icons, SIZES } from "../../../constants";
 import styles from "./popularjobs.style";
+import { isLoading } from "expo-font";
 
 const Popularjobs = () => {
   const router = useRouter();
+  const isLoading=false;
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Popularjobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View> 
+
+      <View style={styles.cardsContainer}>
+        {isLoading?(<ActivityIndicator size="large" color={COLORS.primary}/>):(</>)}
       </View>
     </View>
   );
