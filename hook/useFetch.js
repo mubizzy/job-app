a67072f3d7792298fa4dafafbbb1fb0087297180
Mubarak <ajibolamubarak304@gmail.com>
2023-04-01 11:28:59 -0500

    Api integration

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -5,4 +5,18 @@ const useFetch = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: "https://jsearch.p.rapidapi.com/search",
+    params: {
+      query: "Python developer in Texas, USA",
+      page: "1",
+      num_pages: "1",
+    },
+    headers: {
+      "X-RapidAPI-Key": "c9aa265fffmshd1557fa87b9cdcbp121324jsn0efa758ebf27",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+  };
 };
