616db70788d66f606ffe034f457857e01f498c32
Mubarak <ajibolamubarak304@gmail.com>
2023-04-01 11:50:03 -0500

    Api integration3

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,22 +1,21 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
+// import env from "react-native-dotenv";
+import { RAPID_API_KEY } from "@env";
 
-const useFetch = () => {
+const rapidapikey = RAPID_API_KEY;
+const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const options = {
     method: "GET",
-    url: "https://jsearch.p.rapidapi.com/search",
-    params: {
-      query: "Python developer in Texas, USA",
-      page: "1",
-      num_pages: "1",
-    },
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": "c9aa265fffmshd1557fa87b9cdcbp121324jsn0efa758ebf27",
+      "X-RapidAPI-Key": rapidapikey,
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
+    params: { ...query },
   };
 };
