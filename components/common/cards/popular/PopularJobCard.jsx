473275d92772744f26118b4badb66b73e8b54d9a
Mubarak <ajibolamubarak304@gmail.com>
2023-04-02 07:13:54 -0600

    Api integration7

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from "react";
+import { View, Text } from "react-native";
 
-import styles from './popularjobcard.style'
+import styles from "./popularjobcard.style";
+import { TouchableOpacity } from "react-native-web";
 
-const PopularJobCard = () => {
+const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
-    <View>
-      <Text>PopularJobCard</Text>
-    </View>
-  )
-}
+    <TouchableOpacity>
+      style={styles.container(selectedJob, item)}
+      onPress={() => handleCardPress(item)}
+    </TouchableOpacity>
+  );
+};
 
-export default PopularJobCard
+export default PopularJobCard;
