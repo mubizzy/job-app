e89fbd39d7e1faf39ff5359fcd6326e5856f4f6d
Mubarak <ajibolamubarak304@gmail.com>
2023-04-02 16:40:33 -0600

    Api integration8

--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -1,14 +1,26 @@
 import React from "react";
-import { View, Text } from "react-native";
-
+import { View, Text, TouchableOpacity, Image } from "react-native";
+// import { checkImageURL } from "../../../../utils";
 import styles from "./popularjobcard.style";
-import { TouchableOpacity } from "react-native-web";
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity
       style={styles.container(selectedJob, item)}
       onPress={() => handleCardPress(item)}
+    >
+      <TouchableOpacity style={styles.logoContainer(selectedJob, item)}>
+        <Image
+          source={{
+            uri: item.employer_logo,
+          }}
+          resizeMode="contain"
+          style={styles.logoImage}
+        />
+      </TouchableOpacity>
+      <Text style={styles.companyName} numberOfLines={1}>
+        {item.employer_name}
+      </Text>
     </TouchableOpacity>
   );
 };
