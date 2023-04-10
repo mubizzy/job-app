f610c0b5717c3f3e457b7ae99fc8829dfe01e5e9
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2023-04-10 13:41:59 -0800

    second page

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -22,6 +22,18 @@ import useFetch from "../../hook/useFetch";
 
 const jobDetails = () => {
   const router = useRouter();
+  const params = useSearchParams();
+  const { data, isLoading, error, refetch } = useFetch("job-details", {
+    job_id: params.id,
+  });
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    refetch();
+    setRefreshing(false);
+  }, []);
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       {" "}
@@ -43,6 +55,31 @@ const jobDetails = () => {
           headerTitle: "",
         }}
       />
+      <>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          refreshControl={
+            <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          }
+        >
+          {isLoading ? (
+            <ActivityIndicator size="large" color={COLORS.primary} />
+          ) : error ? (
+            <Text>Something went wrong</Text>
+          ) : data.length === 0 ? (
+            <Text>No data available</Text>
+          ) : (
+            <View style={{ padding: SIZES.medium, paddingBottom: 100 }}>
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                location={data[0].job_country}
+              />
+            </View>
+          )}
+        </ScrollView>
+      </>
     </SafeAreaView>
   );
 };
